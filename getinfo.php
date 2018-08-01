2b920daf4be9befaca014aa0051f5938c16e0d89
Bjørn Johansen <bjorn@dekode.no>
2018-08-01 10:56:17 +0200

    Get the domain name from the domain table.

--- getinfo.php
+++ getinfo.php
@@ -18,8 +18,13 @@ defined( 'ABSPATH' ) || exit;
 
 add_action(
 	'template_redirect', function() {
+
+		global $wpdb, $current_blog;
+
 		// $info = is_ssl() ? 'https' : 'http'; // Works.
-		$info = domain_mapping_siteurl( false );
+		// $info = domain_mapping_siteurl( false ); // Returns with http://
+		$info = $wpdb->get_var( "SELECT domain FROM {$wpdb->dmtable} WHERE blog_id = '{$wpdb->blogid}' AND active = 1 LIMIT 1" );
+
 		header( 'X-Info: ' . $info );
 	}, 9
 );
